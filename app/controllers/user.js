03ac6ecbef6c2d60d0f3bd81b7011d21baf77ce2
davidrol6 <davidrol6@hotmail.com>
2017-02-01 23:19:48 +0100

    -- Tweaks

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -64,7 +64,7 @@ exports.forgot = function(req,res, next){
                       subject: 'Password Reset player.aprendecondedos.es',
                       text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                       'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                      'http://' + req.headers.origin + '/reset/' + token + '\n\n' +
+                      '' + req.headers.origin + '/reset/' + token + '\n\n' +
                       'If you did not request this, please ignore this email and your password will remain unchanged.\n'
                   };
 
